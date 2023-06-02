aaec40947bd92048266d38531e7a3146f33fc4bc
Zaffer Ali <87249334+zafferali@users.noreply.github.com>
2023-06-02 14:47:05 +0530

    fetch data from torre api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
-# Create your views here.
+@api_view(['GET'])
+def getUser(request):
+    getUserUrl = "https://torre.bio/api/bios/zafferali" 
+    response = requests.get(getUserUrl)
+    data = response.json()
+    print(data)
+    return Response(data)
